d1bb7e562328d6a638134b686a630790eb39fbf0
fstemarie <fstemarie@gmail.com>
2021-07-20 01:30:24 -0400

    Moved constants to 3_Preparations.js. Removed global variables

--- src/Code.js
+++ src/Code.js
@@ -1,23 +1,11 @@
-const LABEL_UNPROCESSED = "Horaire",
-  LABEL_PROCESSED = "Processed",
-  FOLDER_UNPROCESSED = "Unprocessed",
-  FOLDER_PROCESSED = "Processed",
-  FOLDER_ROOT = "Horaire"
-  MYCALNAME = "Best buy - Francois",
-  MATESCALNAME = " Best buy - Collegues",
-  REGISTRYNAME = "Horaire - Registry"
-
-var startTs, endTs, duration, myCal, matesCal,
-  registry, rootFolder, unprocFolder, procFolder
-
 function updateCalendars_tt() {
-  startTs = moment()
+  globalThis.startTs = moment()
   Logger.clear()
   Logger.log("-- prepareSheets_tt() " + startTs)
   prepare()
   let msgInfos = prepareSheets()
   processSheets(msgInfos)
   fillCalendars()
-  endTs = moment()
+  globalThis.endTs = moment()
   Logger.log("Execution Time: " + endTs.diff(startTs, "seconds") + "sec")
 }
