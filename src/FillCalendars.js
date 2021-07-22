36508ccd440e15996e4ca46831f8701853eb2eab
fstemarie <fstemarie@gmail.com>
2021-07-21 22:44:39 -0400

    Refactoring. Changed calendar colors

--- src/FillCalendars.js
+++ src/FillCalendars.js
@@ -1,19 +1,25 @@
 function fillCalendars() {
   Logger.log("-- fillCalendars()")
   for (const sheet of registry.getSheets()) {
-    if (moment().diff(startTs, "seconds") >= 350) {
-      break
-    }
     let sheetName = sheet.getName()
     if (sheetName === "TEMPLATE") continue
-    let cal = CalendarApp.getCalendarsByName(sheetName)[0]
-    if (!cal) cal = CalendarApp.createCalendar(sheetName)
+    let cal = CalendarApp.getCalendarsByName("gs-" + sheetName)[0]
+    if (!cal) {
+      cal = CalendarApp.createCalendar("gs-" + sheetName)
+      if (sheetName == "ste-marie-francois") {
+        cal.setColor(CalendarApp.Color.INDIGO)
+      } else {
+        cal.setColor(CalendarApp.Color.PURPLE)
+      }
+      Logger.log("Pause 1s...")
+      Utilities.sleep(1000)
+    }
     fillCalendar(cal, sheet.sort(4))
   }
 }
 
 function fillCalendar(cal, sheet) {
-  Logger.log("-- fillCalendar()")
+  Logger.log("-- fillCalendar(" + cal.getName() + ", " + sheet.getName() + ")")
   let regRange, regEvents, event, employee
   let eventName, eventStart, eventEnd, eventId
   regRange = sheet.getDataRange().offset(1, 0)
@@ -25,11 +31,11 @@ function fillCalendar(cal, sheet) {
     const row = regEvents[row_i]
     if (row[5] == 1) {
       Logger.log("Rangee deja traitee. On la saute")
-      continue; // Si deja traite, on le saute.
+      continue; // Si deja traitee, on la saute.
     }
     if (row[0].length == 0) {
       Logger.log("Rangee vide. On la saute")
-      continue // Si rangee vide, on le saute
+      continue // Si rangee vide, on la saute
     }
     [employee, eventName, eventId, eventStart, eventEnd,] = row
     eventStart = moment(eventStart).toDate()
@@ -45,7 +51,7 @@ function fillCalendar(cal, sheet) {
           event.setColor(CalendarApp.EventColor.ORANGE)
         }
         else {
-          event.setColor(CalendarApp.EventColor.PALE_BLUE)
+          event.setColor(CalendarApp.EventColor.BLUE)
         }
         break
 
@@ -56,11 +62,12 @@ function fillCalendar(cal, sheet) {
           event.setColor(CalendarApp.EventColor.MAUVE)
         }
         else {
-          event.setColor(CalendarApp.EventColor.PALE_RED)
+          event.setColor(CalendarApp.EventColor.RED)
         }
         break
     }
     row[5] = 1 // Signale comme quoi il a ete traite dans le registre
+    row[2] = event.getId()
     Logger.log("Mise a jour du registre...")
     regRange.setValues(regEvents)
     Logger.log("Pause 500ms...")
