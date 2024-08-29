e92cacc4cb8669b15593efeaa8f313f0e049f2ee
fstemarie <14352349+fstemarie@users.noreply.github.com>
2024-08-29 10:32:32 -0400

    Moved registry creation to later in the process

--- src/3_Preparations.js
+++ src/3_Preparations.js
@@ -1,74 +1,56 @@
-const LABEL_UNPROCESSED = "Horaire",
-  LABEL_PROCESSED = "Processed",
-  FOLDER_UNPROCESSED = "Unprocessed",
-  FOLDER_PROCESSED = "Processed",
-  FOLDER_ROOT = "Horaire"
-  MYCALNAME = "Best buy - Francois",
-  MATESCALNAME = " Best buy - Collegues",
-  REGISTRYNAME = "Horaire - Registre"
+const REGISTRYNAME = "Registre"
+
+// Name Gmail label names
+const LABEL_SCHEDULE = "Horaire"
+const LABEL_PROCESSED = "Processed"
+const GMAIL_QUERY = `label:${LABEL_SCHEDULE} AND NOT label:${LABEL_PROCESSED}`
+
+// Names of Google Drive folders
+const FOLDER_ROOT = "Horaire"
+const FOLDER_NEW = "1- New"
+const FOLDER_PREPARED = "2- Prepared"
+const FOLDER_PROCESSED = "3- Processed"
+
+// Names of Google Calendars
+const CAL_ME = "gs-francois"
+const CAL_MATES = "gs-collegues"
 
 ! function() {
   moment.tz.setDefault("America/Vancouver");
   prepareFolders()
-  prepareRegistry()
 }()
 
 function prepareFolders() {
-  Logger.log("-- prepareFolders()")
+  // Logger.log("-- prepareFolders()")
   let folders = DriveApp.getFoldersByName(FOLDER_ROOT)
   if (folders.hasNext()) {
     globalThis.rootFolder = folders.next()
   } else {
+    Logger.log("Creating root folder")
     globalThis.rootFolder = DriveApp.createFolder(FOLDER_ROOT)
+  }
+
+  folders = rootFolder.getFoldersByName(FOLDER_NEW)
+  if (folders.hasNext()) {
+    globalThis.newFolder = folders.next()
+  } else {
+    Logger.log(`Creating \'${FOLDER_NEW}\' folder`)
+    globalThis.newFolder = rootFolder.createFolder(FOLDER_NEW)
+  }
+
+  folders = rootFolder.getFoldersByName(FOLDER_PREPARED)
+  if (folders.hasNext()) {
+    globalThis.unprocFolder = folders.next()
+  } else {
+    Logger.log(`Creating \'${FOLDER_PREPARED}\' folder`)
+    globalThis.unprocFolder = rootFolder.createFolder(FOLDER_PREPARED)
   }
 
   folders = rootFolder.getFoldersByName(FOLDER_PROCESSED)
   if (folders.hasNext()) {
     globalThis.procFolder = folders.next()
   } else {
+    Logger.log(`Creating \'${FOLDER_PROCESSED}\' folder`)
     globalThis.procFolder = rootFolder.createFolder(FOLDER_PROCESSED)
   }
-
-  folders = rootFolder.getFoldersByName(FOLDER_UNPROCESSED)
-  if (folders.hasNext()) {
-    globalThis.unprocFolder = folders.next()
-  } else {
-    globalThis.unprocFolder = rootFolder.createFolder(FOLDER_UNPROCESSED)
-  }
-}
-
-function createRegistry() {
-  Logger.log("-- createRegistry()")
-  let spreadsheet = SpreadsheetApp.create(REGISTRYNAME)
-  DriveApp.getFileById(spreadsheet.getId()).moveTo(rootFolder)
-  let sheet = spreadsheet.getSheets()[0]
-  sheet.setName("Registre")
-  sheet.appendRow([
-    "Employé",
-    "Sommaire",
-    "Début",
-    "Fin",
-    "Erreur",
-    "Original",
-    "Traité",
-    "Id",
-    "Horodate Execution"
-  ])
-  sheet.getRange("A1:I1").setFontSize(14).setFontWeight("bold")
-    .setHorizontalAlignment("center").setBorder(null, null, true,
-      null, null, null)
-  sheet.setFrozenRows(1)
-  return spreadsheet
-}
-
-function prepareRegistry() {
-  Logger.log("-- prepareRegistry()")
-  let files = rootFolder.getFilesByName(REGISTRYNAME)
-  let registry
-  if (files.hasNext()){
-    registry = SpreadsheetApp.open(files.next())
-  } else {
-    registry = createRegistry()
-  }
-  globalThis.registry = registry
 }
